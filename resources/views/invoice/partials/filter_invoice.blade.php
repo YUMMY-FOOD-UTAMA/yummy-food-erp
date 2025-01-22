779c374b1a0773403de262b6885aadcd0201c5e4
iban.rama <ibanrama29@gmail.com>
2025-01-22 22:22:09 +0700

    added default value start and end date

--- resources/views/invoice/partials/filter_invoice.blade.php
+++ resources/views/invoice/partials/filter_invoice.blade.php
@@ -1,5 +1,7 @@
 <x-table.advance-filter using-apply-button="true" class="mt-5 ms-6 me-6">
-    <x-form.input-daterange label="Date" class="col-12 col-md-3 mb-5" type="row" size-form="sm"/>
+    <x-form.input-daterange label="Date" class="col-12 col-md-3 mb-5" type="row" size-form="sm"
+                            :default-value-start-date="request()->start_date"
+                            :default-value-end-date="request()->end_date"/>
     <x-data-driven.select2.customer-invoice size-form="sm" class="col-12 col-md-3 mb-5" type="row"
                                             :customer-invoice-i-d="request('customer_invoice_id')"
                                             label="Customer Account Name" account-name="true"
