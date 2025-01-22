2c53e1a018a64638060f3826194db2731d9597d8
iban.rama <ibanrama29@gmail.com>
2025-01-22 22:26:53 +0700

    restore scripts

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
-#
-#echo "Deployment started ..."
-#
-#(php artisan down) || true
-#
-#git pull origin main
-#
-#composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+echo "Deployment started ..."
+
+(php artisan down) || true
+
+git pull origin main
+
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 
 php artisan clear-compiled
 
@@ -20,24 +20,24 @@ php artisan route:cache
 php artisan view:clear
 php artisan view:cache
 
-#php artisan optimize
-#
-#commit_message=$(git log -1 --pretty=%B)
-#if [[ "$commit_message" == *"--migrate"* ]]; then
-#    echo "Running migrations..."
-#    php artisan migrate
-#else
-#    echo "No migrations required."
-#fi
-#
-#if [[ "$LAST_COMMIT_MSG" == *"--seed="* ]]; then
-#  SEED_NAME=$(echo "$LAST_COMMIT_MSG" | sed -n 's/.*--seed=\([^ ]*\).*/\1/p')
-#  echo "Seeding detected. Running seeder: $SEED_NAME..."
-#  php artisan db:seed --class="$SEED_NAME"
-#else
-#  echo "No seeding required in last commit."
-#fi
-#
-#php artisan up
-#
-#echo "Deployment finished!"
+php artisan optimize
+
+commit_message=$(git log -1 --pretty=%B)
+if [[ "$commit_message" == *"--migrate"* ]]; then
+    echo "Running migrations..."
+    php artisan migrate
+else
+    echo "No migrations required."
+fi
+
+if [[ "$LAST_COMMIT_MSG" == *"--seed="* ]]; then
+  SEED_NAME=$(echo "$LAST_COMMIT_MSG" | sed -n 's/.*--seed=\([^ ]*\).*/\1/p')
+  echo "Seeding detected. Running seeder: $SEED_NAME..."
+  php artisan db:seed --class="$SEED_NAME"
+else
+  echo "No seeding required in last commit."
+fi
+
+php artisan up
+
+echo "Deployment finished!"
