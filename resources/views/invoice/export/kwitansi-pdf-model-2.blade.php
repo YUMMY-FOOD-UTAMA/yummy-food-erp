a52615e21a91a7120ecab1bfbbf7eb7a798b2806
iban.rama <94698720+SyaibanAhmadRamadhan@users.noreply.github.com>
2025-02-06 11:04:35 +0700

    Update kwitansi-pdf-model-2.blade.php

--- resources/views/invoice/export/kwitansi-pdf-model-2.blade.php
+++ resources/views/invoice/export/kwitansi-pdf-model-2.blade.php
@@ -7,7 +7,7 @@
     <title>Kwitansi</title>
     <style>
         @page {
-            margin-top: 3cm;
+            margin-top: 1.5cm;
             margin-bottom: 1.5cm; /* Atau 56.69px */
             margin-left: 1.5cm;
             margin-right: 1.5cm; /* Minimum 1.5cm */
